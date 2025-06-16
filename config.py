bc9ebece2c8c70017909504e840c1cb754ea6d73
saikat8509 <mailmetosaikat007@gmail.com>
2025-06-16 09:40:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ from urllib.parse import quote_plus
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # API ID and Hash from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "23584757"))
+API_HASH = os.environ.get("API_HASH", "ac9926d2cb8acc38413f5e93881fd514")
 
 # Admin ID for broadcast
-ADMINS = int(os.environ.get("ADMINS", "0"))
+ADMINS = int(os.environ.get("ADMINS", "7616562927"))
 
 # MongoDB credentials
-MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "")
-MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "")
-MONGO_CLUSTER = os.environ.get("MONGO_CLUSTER", "")
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "creazysaikat")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "Saikat9735")
+MONGO_CLUSTER = os.environ.get("MONGO_CLUSTER", "cluster0.y2ku5.mongodb.net")
 
 # Encode MongoDB credentials
 encoded_username = quote_plus(MONGO_USERNAME)
@@ -25,7 +25,7 @@ DB_URI = os.environ.get("DB_URI", f"mongodb://{encoded_username}:{encoded_passwo
 DB_NAME = os.environ.get("DB_NAME", "save_restricted_bot")
 
 # Target channel for forwarding content
-TARGET_CHANNEL = os.environ.get("TARGET_CHANNEL", "")
+TARGET_CHANNEL = os.environ.get("TARGET_CHANNEL", "-1002447367705")
 
 # Enable error messages in user chats
 ERROR_MESSAGE = bool(os.environ.get("ERROR_MESSAGE", True))
