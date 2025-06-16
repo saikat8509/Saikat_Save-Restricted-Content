c448be906496ecee6e5d4bad1cc3783cefbd5338
saikat8509 <mailmetosaikat007@gmail.com>
2025-06-16 10:56:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-  app = Flask(__name__)
+app = Flask(__name__)
 
-  @app.route('/')
-  def health_check():
-      return "Bot is running", 200
+@app.route('/')
+def health_check():
+    return "Bot is running", 200
 
-  if __name__ == "__main__":
-      app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
